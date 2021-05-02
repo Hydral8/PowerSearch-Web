182e49d9cd8898cc90e8f9540e95bdb52c16aa57
Hydral8 <sbae703@gmail.com>
2021-05-02 15:10:02 -0700

    new commit

--- src/services/Firebase.js
+++ src/services/Firebase.js
@@ -3,17 +3,17 @@ import firebase from "firebase";
 console.log(process.env.REACT_APP_API_KEY);
 
 let firebaseConfig =
-  process.env.NODE_ENV == "production"
-    ? process.env.firebaseServiceAccount
-    : {
-        apiKey: process.env.REACT_APP_API_KEY,
-        authDomain: "power-search-8e45f.firebaseapp.com",
-        projectId: "power-search-8e45f",
-        storageBucket: "power-search-8e45f.appspot.com",
-        messagingSenderId: "637151128575",
-        appId: "1:637151128575:web:27b379c2aab0a4e3fad5ca",
-        measurementId: "G-3SZ3LHCFX2",
-      };
+  //   process.env.NODE_ENV == "production"
+  //     ? process.env.firebaseServiceAccount :
+  {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: "power-search-8e45f.firebaseapp.com",
+    projectId: "power-search-8e45f",
+    storageBucket: "power-search-8e45f.appspot.com",
+    messagingSenderId: "637151128575",
+    appId: "1:637151128575:web:27b379c2aab0a4e3fad5ca",
+    measurementId: "G-3SZ3LHCFX2",
+  };
 
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
